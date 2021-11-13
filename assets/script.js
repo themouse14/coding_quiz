2ac56e729059d9eff33a5549f9793b0f7dd6121d
themouse14 <themouse.ow@gmail.com>
2021-11-13 09:44:59 -0700

    rearranged code, added a few notes

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,38 @@
+// want to immediately run this function
+function buildQuiz() {
+    const output = [];
+    myQuestions.forEach(
+        (currentQuestion, questionNumber) => {
+            const answers = [];
+            for(letter in currentQuestion.answers){
+                answers.push(
+                    `<label>
+                        <input type="radio" name='question${questionNumber}' value='${letter}'></input>
+                        ${letter} :
+                        ${currentQuestion.answers[letter]}
+                    </label>`
+                );
+            }
+            output.push(
+                `<div class="question"> ${currentQuestion.question}</div>
+                <div class="answers"> ${answers.join('')}</div>`
+            );
+        }
+    );    
+    quizContainer.innerHTML = output.join('');
+}
+
+// this functions runs on click
+function showResults() {
+
+}
+
 const quizContainer = $('#quiz');
 const resultsContainer = $('#results');
 const submitButton = $('#submit');
 
 // storing quiz questions, sorted in an array
+
 const myQuestions = [
     {
     question: "What does CSS stand for?",
@@ -44,38 +74,9 @@ const myQuestions = [
     },
     correctAnswer: "b",
     }
-];
+]
 
-// want to immediately run this function
-function buildQuiz() {
-    const output = [];
-    myQuestions.forEach(
-        (currentQuestion, questionNumber) => {
-            const answers = [];
-            for(letter in currentQuestion.answers){
-                answers.push(
-                    `<label>
-                        <input type="radio" name='question${questionNumber}' value='${letter}'></input>
-                        ${letter} :
-                        ${currentQuestion.answers[letter]}
-                    </label>`
-                );
-            }
-            output.push(
-                `<div class="question"> ${currentQuestion.question}</div>
-                <div class="answers"> ${answers.join('')}</div>`
-            );
-        }
-    );    
-    quizContainer.innerHTML = output.join('');
-}
-
-// this functions runs on click
-function showResults() {
-
-}
-
-
+//run this function
 buildQuiz();
-
+// add event listener
 submitButton.addEventListener('click', showResults);
