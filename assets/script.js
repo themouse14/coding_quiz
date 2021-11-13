a825caef1649149fb5a15bc16186441c698cec2c
themouse14 <themouse.ow@gmail.com>
2021-11-13 08:19:38 -0700

    started layout and outline of js and html

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+const quizContainer = $('#quiz');
+const resultsContainer = $('#results');
+const submitButton = $('#submit');
+
+// displaying quiz functions
+
+
+// want to run this function immediately
+function buildQuiz() {
+
+}
+
+// this functions runs on click
+function showResults() {
+
+}
