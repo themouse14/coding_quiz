86bd4f40fc671ccfa91498acaa40248471d0ea2f
themouse14 <themouse.ow@gmail.com>
2021-11-13 08:49:53 -0700

    added const myQuestions to store all questions and answers

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,49 @@ const quizContainer = $('#quiz');
 const resultsContainer = $('#results');
 const submitButton = $('#submit');
 
-// displaying quiz functions
-
+// displaying quiz functions, sorted in an array
+const myQuestions = [
+    {
+    question: "What does CSS stand for?",
+    answers: {
+        a: "Constant Secondary Solutions",
+        b: "Cascading Style Sheet",
+        c: "Clown-nose Standard Spray",
+        d: "Casting Style Set-ups"
+        },
+    correctAnswer: "b",
+    },
+    {
+    question: "What is possible with CSS?",
+    answers: {
+        a: "Changing font, margins, and borders",
+        b: "Adapting screen size based on the device being used",
+        c: "Positioning elements such as images on the page",
+        d: "All of the above!"
+        },
+    correctAnswer: "d",
+    },
+    {
+    question: "What is Bootstrap?",
+    answers: {
+        a: "A large library of images not protected by copyright",
+        b: "An agency that protects software developers from theft",
+        c: "The world's most opoluar front-end open source toolkit",
+        d: "A boss in the video game series Spyro"
+        },
+    correctAnswer: "c",
+    },
+    {
+    question: "Which of the following is NOT a data type?",
+    answers: {
+        a: "Boolean",
+        b: "Alert",
+        c: "String",
+        d: "Number"
+    },
+    correctAnswer: "b",
+    }
+];
 
 // want to run this function immediately
 function buildQuiz() {
