12573aece64236e8070e0d47937a4f0249fef193
themouse14 <themouse.ow@gmail.com>
2021-11-13 09:21:36 -0700

    added jquery link to html, completed buildQuiz function in js

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@ const quizContainer = $('#quiz');
 const resultsContainer = $('#results');
 const submitButton = $('#submit');
 
-// displaying quiz functions, sorted in an array
+// storing quiz questions, sorted in an array
 const myQuestions = [
     {
     question: "What does CSS stand for?",
@@ -46,12 +46,36 @@ const myQuestions = [
     }
 ];
 
-// want to run this function immediately
+// want to immediately run this function
 function buildQuiz() {
-
+    const output = [];
+    myQuestions.forEach(
+        (currentQuestion, questionNumber) => {
+            const answers = [];
+            for(letter in currentQuestion.answers){
+                answers.push(
+                    `<label>
+                        <input type="radio" name='question${questionNumber}' value='${letter}'></input>
+                        ${letter} :
+                        ${currentQuestion.answers[letter]}
+                    </label>`
+                );
+            }
+            output.push(
+                `<div class="question"> ${currentQuestion.question}</div>
+                <div class="answers"> ${answers.join('')}</div>`
+            );
+        }
+    );    
+    quizContainer.innerHTML = output.join('');
 }
 
 // this functions runs on click
 function showResults() {
 
 }
+
+
+buildQuiz();
+
+submitButton.addEventListener('click', showResults);
